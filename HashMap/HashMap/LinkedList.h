418ddd7da0976e3e786a9364f889bb4e8131d778
Dnelre-Slin <nilsheggem@hotmail.com>
2017-11-03 15:42:10 +0100

    LinkedList done.

--- HashMap/HashMap/LinkedList.h
+++ HashMap/HashMap/LinkedList.h
@@ -1,4 +1,5 @@
 #pragma once
+#include <iostream>
 
 template <class KEY, class VALUE>
 class LinkedList
@@ -7,8 +8,9 @@ private:
 	LinkedList *m_next;
 	KEY m_key;
 	VALUE m_value;
+	bool m_set;
 public:
-	LinkedList(const KEY &key, const VALUE &value);
+	LinkedList();
 	~LinkedList();
 
 	void append(const KEY &key, const VALUE &value);
@@ -16,15 +18,44 @@ public:
 };
 
 template<class KEY, class VALUE>
-inline LinkedList<KEY, VALUE>::LinkedList(const KEY &key, const VALUE &value)
+inline LinkedList<KEY, VALUE>::LinkedList()
 {
 	m_next = 0;
-	m_key = key;
-	m_value = value;
+	m_set = false;
 }
 
 template<class KEY, class VALUE>
 inline LinkedList<KEY, VALUE>::~LinkedList()
 {
+	delete m_next;
+}
 
+template<class KEY, class VALUE>
+inline void LinkedList<KEY, VALUE>::append(const KEY &key, const VALUE &value)
+{
+	if (!m_set)
+	{
+		m_key = key;
+		m_value = value;
+		m_set = true;
+	}
+	else if (m_next == 0)
+	{
+		m_next = new LinkedList;
+		m_next->append(key, value);
+	}
+	else
+	{
+		m_next->append(key, value);
+	}
+}
+
+template<class KEY, class VALUE>
+inline VALUE &LinkedList<KEY, VALUE>::get(const KEY &key)
+{
+	if (m_key == key)
+		return m_value;
+	if (m_next != 0)
+		return m_next->get(key);
+	throw std::runtime_error("Not found.\n");
 }
